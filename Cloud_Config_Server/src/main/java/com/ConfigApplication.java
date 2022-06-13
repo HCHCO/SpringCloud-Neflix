e34c99421cb9f6ad7a31e34eed45afe48d0304e6
Shop <2421864719@qq.com>
2022-06-13 17:03:51 +0800

    D6 config-server

--- Cloud_Config_Server/src/main/java/com/ConfigApplication.java
+++ Cloud_Config_Server/src/main/java/com/ConfigApplication.java
@@ -3,10 +3,12 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class testApplicaiton {
+@EnableConfigServer
+public class ConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(testApplicaiton.class,args);
+        SpringApplication.run(ConfigApplication.class,args);
     }
 }
